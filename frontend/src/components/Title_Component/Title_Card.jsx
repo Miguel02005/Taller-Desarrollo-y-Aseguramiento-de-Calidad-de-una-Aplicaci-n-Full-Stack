d277a824d04c468a35f6ba5e77d57f3af1ea59df
Miguel <miguelangel2005.o.h@gmail.com>
2026-05-30 18:47:10 -0500

    feat: Replace logo with BookIcon component in SmartBook for improved UI

--- frontend/src/components/Title_Component/Title_Card.jsx
+++ frontend/src/components/Title_Component/Title_Card.jsx
@@ -1,14 +1,21 @@
 import './Title_Style.css'
 
+function BookIcon() {
+  return (
+    <svg width="28" height="28" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path d="M4 19.5C4 18.837 4.26339 18.2011 4.73223 17.7322C5.20107 17.2634 5.83696 17 6.5 17H20" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
+      <path d="M6.5 2H20V22H6.5C5.83696 22 5.20107 21.7366 4.73223 21.2678C4.26339 20.7989 4 20.163 4 19.5V4.5C4 3.83696 4.26339 3.20107 4.73223 2.73223C5.20107 2.26339 5.83696 2 6.5 2Z" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
+    </svg>
+  );
+}
+
 export default function SmartBook() {
   return (
     <div className="title-brand">
-      <img
-        src="/Logo.png"
-        alt="Smart Book Finder Logo"
-        className="title-logo"
-      />
-      <span className="title-brand__text">Smart Book</span>
+      <span className="title-icon">
+        <BookIcon />
+      </span>
+      <span className="title-brand__text">Smart Book Finder</span>
     </div>
   )
 }
